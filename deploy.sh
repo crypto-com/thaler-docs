70ca12eb7b06c072727dbba2314e245ed2892e11
Tomas Tauber <2410580+tomtau@users.noreply.github.com>
2019-08-02 09:35:11 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -22,9 +22,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+git push -f git@github.com:crypto-com/crypto-com.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:brianMCO/demo-doc-2.git master:gh-pages
+#git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
